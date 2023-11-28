4f64b3ab40a1721dc9c5038cbfd0360a42f82431
Sergas <sergasgoi@hotmail.com>
2023-11-27 21:11:41 -0300

    5

--- models/consultas.js
+++ models/consultas.js
@@ -31,7 +31,7 @@ async function agregarRegistros(array) {
 
 async function agregarTurno(id_usuario, name) {
 
-    var query = 'insert into turnos(iduser, name , horain, horafin) values(?, ?, NOW(), 0)';
+    var query = 'insert into turnos(iduser, name , horain, horafin, total, condicion) values(?, ?, NOW(), 0, 0, 0)';
     var datos = await pool.query(query, [id_usuario, name]);
     return datos;
 }
